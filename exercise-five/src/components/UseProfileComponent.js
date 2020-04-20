99bf9eff9ddc7755c12837562dc3ce384cdc55e3
chloeych <52624099+chloeych@users.noreply.github.com>
2020-04-20 09:09:21 -0400

    setup components and clean out

---
+++ exercise-five/src/components/UseProfileComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+function UserProfileComponent(){
+    return (
+    <div>
+       <p>
+           User Profile Component
+       </p>
+    </div>
+    );
+}
+
+export default UserProfileComponent;
---
+++ exercise-five/src/containers/UseProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+function UserProfile(){
+    return (
+    <div>
+       <h1>
+        user profile
+       </h1>
+    </div>
+    );
+}
+
+export default UserProfile;
